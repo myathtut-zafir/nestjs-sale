512687ce5e02006b83748b1e8d4e7fa07352cdfc
myathtut-zafir <aa.htut@gmail.com>
2025-09-15 22:29:00 +0700

    feat:setup module auth

---
+++ src/auth/jwt/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  validate(payload: any) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
