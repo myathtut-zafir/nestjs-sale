14dd96e4b25b56da311e792cded85c37272f289a
myathtut-zafir <aa.htut@gmail.com>
2025-09-28 21:59:48 +0700

    fix: syntax

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Product } from './entities/products.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
