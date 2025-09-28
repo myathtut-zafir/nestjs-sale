ea5e48fccca7acb83803da7b446dea2af8055ec6
myathtut-zafir <aa.htut@gmail.com>
2025-09-28 22:25:13 +0700

    feat: add doc for product

--- src/product/dtos/create-product.dto.ts
+++ src/product/dtos/create-product.dto.ts
@@ -1,20 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
 import { Category } from 'src/category/entities/category.entity';
 import { IsExist } from 'src/validation/is-exist.validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'The name of the product',
+    example: 'Nike',
+  })
   @IsString()
   @IsNotEmpty()
   brand: string;
-
+  @ApiProperty({
+    description: 'The sex',
+    example: 'Male or Female',
+  })
   @IsString()
   @IsNotEmpty()
   sex: string;
-
+  @ApiProperty({
+    description: 'The product model',
+    example: 'Air Max 2021',
+  })
   @IsString()
   @IsNotEmpty()
   model: string;
 
+  @ApiProperty({
+    description: 'The category ID',
+    example: 1,
+  })
   @IsNumber()
   @IsExist(Category)
   categoryId: number;
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dtos/create-product.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+  @ApiOperation({ summary: 'Create new product' })
+  @ApiResponse({
+    status: 201,
+    description: 'The product has been successfully created.',
+  })
   @Post('create')
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
