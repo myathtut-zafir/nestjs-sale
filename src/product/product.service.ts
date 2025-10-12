26ac9cbf7a955685792b0512217295e84cdf5e00
myathtut-zafir <aa.htut@gmail.com>
2025-10-12 22:06:58 +0700

    fix: remove category repo

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,7 +3,6 @@ import { Product } from './entities/products.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateProductDto } from './dtos/create-product.dto';
-import { Category } from 'src/category/entities/category.entity';
 import { AuthUserDto } from 'src/iam/dtos/auth-user.dto';
 
 @Injectable()
@@ -11,8 +10,6 @@ export class ProductService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
-    @InjectRepository(Category)
-    private readonly categoryRepository: Repository<Category>,
   ) {}
 
   async create(createProductDto: CreateProductDto, user: AuthUserDto) {
